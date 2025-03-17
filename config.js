ef37d4c40174b712f03b515c124f56d1fb2b0229
gvangalen <gvangalen@hotmail.com>
2025-03-17 21:41:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "http://13.60.235.90:5002";
-export const TECHNICAL_API_URL = "http://13.60.235.90:5003";
-export const CELERY_API_URL = "http://13.60.235.90:5004"; 
+// Configuratie van API URL's
+export const API_BASE_URL = "http://13.60.235.90:5002";      // Basis API voor macro- en marktdata
+export const TECHNICAL_API_URL = "http://13.60.235.90:5003"; // API voor technische data
+export const CELERY_API_URL = "http://13.60.235.90:5004";   // API voor Celery (voor achtergrondprocessen)
