7d7b292410805f09846139e49d5a13ed4a961ca1
gvangalen <gvangalen@hotmail.com>
2025-03-18 21:54:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Configuratie van API URL's
-export const API_BASE_URL = "http://13.60.235.90:5002";      // Basis API voor macro- en marktdata
-export const TECHNICAL_API_URL = "http://13.60.235.90:5003"; // API voor technische data
-export const CELERY_API_URL = "http://13.60.235.90:5004";   // API voor Celery (voor achtergrondprocessen)
+export const API_BASE_URL = "https://market-dashboard-server.duckdns.org";      // Basis API voor macro- en marktdata
+export const TECHNICAL_API_URL = "https://market-dashboard-server.duckdns.org"; // API voor technische data
+export const CELERY_API_URL = "https://market-dashboard-server.duckdns.org";   // API voor Celery (voor achtergrondprocessen)
